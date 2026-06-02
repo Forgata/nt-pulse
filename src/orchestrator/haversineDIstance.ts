f29b99669a09fc2451e99a166794a20cdde43d51
Forgata <gombwadivine@gmail.com>
2026-06-02 15:33:55 +0200

    fix: correct calculation logic in calculateHaversineDistance function

--- src/orchestrator/haversineDIstance.ts
+++ src/orchestrator/haversineDIstance.ts
@@ -5,14 +5,17 @@ export function calculateHaversineDistance(
   lon2: number,
 ): number {
   const EARTH_RADIUS_KM = 6371;
+
   const dLat = ((lat2 - lat1) * Math.PI) / 180;
   const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+  const rLat1 = (lat1 * Math.PI) / 180;
+  const rLat2 = (lat2 * Math.PI) / 180;
+
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(rLat1) * Math.cos(rLat2);
+
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return EARTH_RADIUS_KM * c;
 }
